ccf80064a28b5dfb7f26add0b86cd93dfa600017
yashsagar <sagar.sagar75@gmail.com>
2024-05-18 00:23:01 +0530

    project initial setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-red-400 w-24 h-24"> test</div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootEl = document.getElementById("root");
+const root = ReactDOM.createRoot(rootEl);
+
+root.render(<App />);
